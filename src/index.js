a83136dbca4d26a7186d806e0955668058a00abf
adil-sansey <adil.sansey@gmail.com>
2021-03-10 23:45:45 +0600

    feat: add reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let result;
+  if (n > 0) result = Number(String(n).split('').reverse().join('')); else
+    result = Number(String(-n).split('').reverse().join(''));
+  return result;
 }
